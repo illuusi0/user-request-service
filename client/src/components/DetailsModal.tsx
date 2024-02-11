365142f4b56c536a1ca5b2a226f26364882def21
illuusi0 <ilia.rudenko@yandex.ru>
2024-02-12 00:29:45 +0300

    added sorting

--- client/src/components/DetailsModal.tsx
+++ client/src/components/DetailsModal.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React from 'react'
 
 const RequestDetailsModal: React.FC<{
-  request: {
-    date: string;
-    name: string;
-    type: string;
-    description: string;
-    status: string;
-  } | null;
-  onClose: () => void;
-}> = ({ request, onClose }) => {
-    if (!request) return null;
+    request: {
+        date: string
+        name: string
+        type: string
+        description: string
+        status: string
+    } | null
+    onClose: () => void
+}> = ({ request }) => {
+    if (!request) return null
 
     return (
         <div className="modal">
             <div className="modal-content">
-                <span className="close-button" onClick={onClose}>&times;</span>
                 <h2>Детали обращения</h2>
-                <p><strong>Имя:</strong> {request.name}</p>
-                <p><strong>Тип:</strong> {request.type}</p>
-                <p><strong>Описание:</strong> {request.description}</p>
-                <p><strong>Статус:</strong> {request.status}</p>
+                <p>
+                    <strong>Имя:</strong> {request.name}
+                </p>
+                <p>
+                    <strong>Тип:</strong> {request.type}
+                </p>
+                <p>
+                    <strong>Описание:</strong> {request.description}
+                </p>
+                <p>
+                    <strong>Статус:</strong> {request.status}
+                </p>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default RequestDetailsModal;
+export default RequestDetailsModal
--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import styles from '../styles/Table.module.scss'
+import Modal from './Modal'
+import RequestDetailsModal from './DetailsModal'
 
-// Определяем тип для элемента списка запросов
 interface Request {
     date: string
     name: string
@@ -10,12 +11,49 @@ interface Request {
     status: string
 }
 
-// Определяем пропсы для RequestsTable
 interface RequestsTableProps {
     requests: Request[]
 }
 
 export const RequestsTable: FC<RequestsTableProps> = ({ requests }) => {
+    const [sortConfig, setSortConfig] = useState<{ key: keyof Request; direction: 'ascending' | 'descending' }>({
+        key: 'date',
+        direction: 'descending',
+    })
+
+    const sortedRequests = [...requests].sort((a, b) => {
+        const { key, direction } = sortConfig
+
+        let comparison = 0
+        if (key === 'date') {
+            const dateA = new Date(a.date.split('.').reverse().join('-')).getTime()
+            const dateB = new Date(b.date.split('.').reverse().join('-')).getTime()
+            comparison = dateA - dateB
+        } else {
+            const aValue = a[key] as string
+            const bValue = b[key] as string
+            comparison = aValue.localeCompare(bValue)
+        }
+
+        return direction === 'ascending' ? comparison : -comparison
+    })
+
+    const requestSort = (key: keyof Request) => {
+        let direction: 'ascending' | 'descending' = 'ascending'
+        if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending'
+        }
+        setSortConfig({ key, direction })
+    }
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [selectedRequest, setSelectedRequest] = useState<Request | null>(null)
+
+    const handleRequestClick = (request: Request) => {
+        setSelectedRequest(request)
+        setIsModalOpen(true)
+    }
+
     const getStatusClassName = (status: string) => {
         switch (status) {
             case 'Готово':
@@ -30,37 +68,44 @@ export const RequestsTable: FC<RequestsTableProps> = ({ requests }) => {
     }
 
     return (
-        <table className={styles.tableContent}>
-            <thead>
-                <tr>
-                    <th>
-                        Дата <span> &#9660; </span>
-                    </th>
-                    <th>
-                        Имя <span> &#9660; </span>
-                    </th>
-                    <th>Описание </th>
-                    <th>
-                        Тип <span> &#9660; </span>
-                    </th>
-                    <th>
-                        Статус <span> &#9660; </span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {requests.map((request, index) => (
-                    <tr key={index} className={styles.tableBody}>
-                        <td>{request.date}</td>
-                        <td>{request.name}</td>
-                        <td>{request.description}</td>
-                        <td>{request.type}</td>
-                        <td>
-                            <span className={getStatusClassName(request.status)}>{request.status}</span>
-                        </td>
+        <>
+            <table className={styles.tableContent}>
+                <thead>
+                    <tr>
+                        <th onClick={() => requestSort('date')}>
+                            Дата <span>&#9660;</span>
+                        </th>
+                        <th onClick={() => requestSort('name')}>
+                            Имя <span>&#9660;</span>
+                        </th>
+                        <th>Описание</th>
+                        <th onClick={() => requestSort('type')}>
+                            Тип <span>&#9660;</span>
+                        </th>
+                        <th onClick={() => requestSort('status')}>
+                            Статус <span>&#9660;</span>
+                        </th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {sortedRequests.map((request, index) => (
+                        <tr key={index} className={styles.tableBody}>
+                            <td>{request.date}</td>
+                            <td>{request.name}</td>
+                            <td onClick={() => handleRequestClick(request)}>{request.description}</td>
+                            <td>{request.type}</td>
+                            <td>
+                                <span className={getStatusClassName(request.status)}>{request.status}</span>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            {isModalOpen && selectedRequest && (
+                <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                    <RequestDetailsModal request={selectedRequest} onClose={() => setIsModalOpen(false)} />
+                </Modal>
+            )}
+        </>
     )
 }
